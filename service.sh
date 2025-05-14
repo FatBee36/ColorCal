553b40bb6b861fac238f1135a3a3e6eecfd99aa4
FatBee36 <104758819+FatBee36@users.noreply.github.com>
2025-05-14 10:10:48 +0100

    Update service.sh

--- service.sh
+++ service.sh
@@ -5,8 +5,8 @@ done
 sleep 5
 
 service call SurfaceFlinger 1023 i32 1
-service call SurfaceFlinger 1015 i32 1 f 1.1 f -0.35 f -0.2 f 0 f 0 f 1.1 f -0.3 f 0 f -0.3 f 0 f 1.35 f 0 f 0 f 0 f 0 f 1 
-service call SurfaceFlinger 1022 f 0.8
+service call SurfaceFlinger 1015 i32 1 f 1.1 f -0.18 f -0.1 f 0 f 0 f 1.1 f -0.15 f 0 f -0.15 f 0 f 1.35 f 0 f 0 f 0 f 0 f 1
+service call SurfaceFlinger 1022 f 1.1
 
 settings put system max_refresh_rate 120.0
 settings put system min_refresh_rate 60.0
