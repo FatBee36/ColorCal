28a133da3c99b5dc8f6bd81fe09143eb822c57a2
FatBee36 <104758819+FatBee36@users.noreply.github.com>
2025-05-14 07:35:16 +0100

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,12 +1,12 @@
 start panel_color
 while [ "$(getprop sys.boot_completed)" != "1" ]; do
-    sleep 10
+    sleep 5
 done
-sleep 10
+sleep 5
 
 service call SurfaceFlinger 1023 i32 1
-service call SurfaceFlinger 1015 i32 1 f 1.1 f -0.35 f -0.15 f 0 f -0.2 f 1.15 f -0.1 f 0 f -0.1 f 0 f 1.2 f 0 f 0 f 0 f 0 f 1 
-service call SurfaceFlinger 1022 f 1.1
+service call SurfaceFlinger 1015 i32 1 f 1.1 f -0.35 f -0.2 f 0 f 0 f 1.1 f -0.3 f 0 f -0.3 f 0 f 1.35 f 0 f 0 f 0 f 0 f 1 
+service call SurfaceFlinger 1022 f 0.8
 
 settings put system max_refresh_rate 120.0
 settings put system min_refresh_rate 60.0
